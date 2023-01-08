6c5dff840f9147e041f5f238f5a197ec0d0a5aa2
Hneen <hneen.@Hneens-MacBook-Air.local>
2023-01-08 11:54:58 +0300

    carousel done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 //css
-import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
-import './css/style.css';
+import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+import "./css/style.css";
 
 //js
-import 'webpack-jquery-ui';
-import 'webpack-jquery-ui/css';
-import 'jquery/dist/jquery';  // 1
-import 'popper.js/dist/popper.min'; // 2
-import 'bootstrap/dist/js/bootstrap.min.js'; // 3
+import "webpack-jquery-ui";
+import "webpack-jquery-ui/css";
+import "jquery/dist/jquery"; // 1
+import "popper.js/dist/popper.min"; // 2
+import "bootstrap/dist/js/bootstrap.min.js"; // 3
 
 // font fortawesome
-import '@fortawesome/fontawesome-free/js/all.min';
+import "@fortawesome/fontawesome-free/js/all.min";
 
 $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-      })
+  $('[data-toggle="tooltip"]').tooltip();
+});
